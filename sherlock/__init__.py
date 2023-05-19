ad3d5c8c8e960a039ca2651d99ee0d7945df8df4
Chronie677 <134041922+Chronie677@users.noreply.github.com>
2023-05-19 17:44:19 +1100

    Update __init__.py

--- sherlock/__init__.py
+++ sherlock/__init__.py
@@ -4,3 +4,11 @@ This module contains the main logic to search for usernames at social
 networks.
 
 """
+# clone the repo
+$ git clone https://github.com/sherlock-project/sherlock.git
+
+# change the working directory to sherlock
+$ cd sherlock
+
+# install the requirements
+$ python3 -m pip install -r requirements.txt
